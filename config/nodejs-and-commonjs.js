bef44fca7c8e4b4174794a85213ca72e7374193e
1000ch <shogo.sensui@gmail.com>
2016-12-07 12:47:19 +0900

    Update Node.js & CommonJS

--- config/nodejs-and-commonjs.js
+++ config/nodejs-and-commonjs.js
@@ -1,3 +1,12 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+  // require error handling in callbacks
+  'handle-callback-err': 'error',
+
+  // disallow require calls to be mixed with regular variable declarations
+  'no-mixed-requires': 'error',
+
+  // disallow new operators with calls to require
+  'no-new-require': 'error'
+};
